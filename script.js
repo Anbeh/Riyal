8a4a5be197ec1e362c59486f2a03ba3ef4cdeb4f
🥭 <abolfazlyouseffar@proton.me>
2025-04-27 21:14:55 +0400

    adding gold price + changind some small details

--- script.js
+++ script.js
@@ -6,81 +6,84 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-const CURRENCY_API = 'https://anbeh.github.io/Riyal-api/data.json';
-const CRYPTO_API = 'https://api.cryptorank.io/v0/coins/prices?keys=bitcoin,ethereum,tether,ripple,bnb,solana,usdcoin,dogecoin,cardano,tron&currency=USD';
-
-const currencyMap = {
-  'USD': { name: 'دلار آمریکا', key: 'usd' },
-  'EUR': { name: 'یورو', key: 'eur' },
-  'GBP': { name: 'پوند انگلیس', key: 'gbp' },
-  'AED': { name: 'درهم امارات', key: 'aed' },
-  'TRY': { name: 'لیر ترکیه', key: 'try' },
-  'CNY': { name: 'یوان چین', key: 'cny' },
-  'CAD': { name: 'دلار کانادا', key: 'cad' },
-  'CHF': { name: 'فرانک سوئیس', key: 'chf' },
-  'RUB': { name: 'روبل روسیه', key: 'rub' },
-  'IQD': { name: 'دینار عراق', key: 'iqd' },
-  'JPY': { name: 'ین ژاپن', key: 'jpy' }
-};
-
-const icons = {
-  'USD': 'https://www.emoji.co.uk/files/apple-emojis/flags-ios/1236-flag-of-united-states.png',
-  'EUR': 'https://www.emoji.co.uk/files/apple-emojis/flags-ios/1084-flag-of-european-union.png',
-  'GBP': 'https://www.emoji.co.uk/files/apple-emojis/flags-ios/1235-flag-of-great-britain.png',
-  'AED': 'https://www.emoji.co.uk/files/apple-emojis/flags-ios/1234-flag-of-the-united-arab-emirates.png',
-  'TRY': 'https://www.emoji.co.uk/files/apple-emojis/flags-ios/1228-flag-of-turkey.png',
-  'CNY': 'https://www.emoji.co.uk/files/apple-emojis/flags-ios/1060-flag-of-china.png',
-  'CAD': 'https://www.emoji.co.uk/files/apple-emojis/flags-ios/7164-flag-of-canada.png',
-  'CHF': 'https://www.emoji.co.uk/files/apple-emojis/flags-ios/1217-flag-of-switzerland.png',
-  'RUB': 'https://www.emoji.co.uk/files/apple-emojis/flags-ios/1187-flag-of-russia.png',
-  'IQD': 'https://www.emoji.co.uk/files/apple-emojis/flags-ios/1115-flag-of-iraq.png',
-  'JPY': 'https://www.emoji.co.uk/files/apple-emojis/flags-ios/1121-flag-of-japan.png'
-};
+const CURRENCY_API = 'https://raw.githubusercontent.com/Anbeh/Riyal-api/refs/heads/main/data2.json';
 
 function formatNumber(num) {
-  if (!num || isNaN(num)) return 'N/A';
+  if (num === null || num === undefined || isNaN(num)) return null;
   return new Intl.NumberFormat('en-US').format(Number(num));
 }
 
-function createCurrencyCard(code, price) {
-  const currency = currencyMap[code];
-  const iconUrl = icons[code] || 'default.png';
-
+function createCurrencyCard(currency) {
   return `
     <div class="card">
       <div class="card-header">
-        <img class="crypto-icon" src="${iconUrl}" alt="${code}">
+        <img class="crypto-icon" src="${currency.icon || 'default.png'}" alt="${currency.code}">
         <div class="card-title-group">
           <div class="name">${currency.name}</div>
-          <div class="nickname">${code}</div>
+          <div class="nickname">${currency.code}</div>
         </div>
       </div>
-        <div class="card-footer">
-          <div class="price">
-            ${formatNumber(price)}
-            <img src="toman.png" class="toman-icon" alt="تومان">
-          </div>
+      <div class="card-footer">
+        <div class="price">
+          ${formatNumber(currency.price)}
+          <img src="toman.png" class="toman-icon" alt="تومان">
         </div>
+      </div>
     </div>
   `;
 }
 
+function createCryptoCard(crypto) {
+  return `
+    <div class="card crypto-card">
+      <div class="card-header">
+        <img class="crypto-icon" src="${crypto.icon || 'default.png'}" alt="${crypto.name}">
+        <div class="card-title-group">
+          <div class="name">${crypto.name}</div>
+        </div>
+      </div>
+      <div class="card-footer">
+        <div class="price">$${formatNumber(Number(crypto.price).toFixed(2))}</div>
+      </div>
+    </div>
+  `;
+}
+
+function createGoldCard(gold) {
+  const isDollar = gold.title === "انس طلا";
+  const formattedPrice = formatNumber(gold.price_toman);
+
+  return `
+    <div class="card gold-card">
+      <div class="card-header">
+        <img class="crypto-icon" src="${'gold.png'}" alt="${gold.title}">
+        <div class="card-title-group">
+          <div class="name">${gold.title}</div>
+        </div>
+      </div>
+      <div class="card-footer">
+        <div class="price">
+          ${isDollar ? '$' + formattedPrice : formattedPrice + '<img src="toman.png" class="toman-icon" alt="تومان">'}
+        </div>
+      </div>
+    </div>
+  `;
+}
 
 async function loadCurrencyData() {
   try {
     const res = await fetch(CURRENCY_API);
     const data = await res.json();
-    const grid = document.getElementById('currency-grid');
-    
-    const currencyCards = data.selected_rates
-      .map(currency => {
-        if (!currencyMap[currency.code]) return '';
-        return createCurrencyCard(currency.code, currency.price);
-      })
-      .join('');
-    
-    grid.innerHTML = currencyCards;
-    
+
+    const currencyGrid = document.getElementById('currency-grid');
+    currencyGrid.innerHTML = data.currency_rates.map(createCurrencyCard).join('');
+
+    const goldGrid = document.getElementById('gold-grid');
+    goldGrid.innerHTML = data.gold_prices.map(createGoldCard).join('');
+
+    const cryptoGrid = document.getElementById('crypto-grid');
+    cryptoGrid.innerHTML = data.cryptos.map(createCryptoCard).join('');
+
     const date = new Date(data.checked_at);
     const options = { 
       year: 'numeric', 
@@ -94,73 +97,18 @@ async function loadCurrencyData() {
       `${new Intl.DateTimeFormat('en-US', options).format(date)}`;
     
   } catch (err) {
+    console.error('Error loading data:', err);
     document.getElementById('currency-grid').innerHTML = `
-      <div style="color: gray; text-align: center; grid-column: 1/-1">
+      <div style="font-family:"Kalameh"; 3color: gray; text-align: center; grid-column: 1/-1">
         خطا در دریافت داده‌ها
       </div>`;
   }
 }
 
-async function loadCryptoData() {
-  try {
-    const res = await fetch(CRYPTO_API);
-    const data = await res.json();
-    const grid = document.getElementById('crypto-grid');
-    grid.innerHTML = data.data.map(createCryptoCard).join('');
-  } catch (err) {
-    console.error('Error loading crypto data:', err);
-  }
-}
-
-const cryptoAbbreviations = {
-  bitcoin: 'BTC',
-  ethereum: 'ETH',
-  tether: 'USDT',
-  ripple: 'XRP',
-  bnb: 'BNB',
-  solana: 'SOL',
-  usdcoin: 'USDC',
-  dogecoin: 'DOGE',
-  cardano: 'ADA',
-  tron: 'TRX'
-};
-
-const cryptoIcons = {
-  bitcoin: 'https://img.cryptorank.io/coins/60x60.bitcoin1524754012028.png',
-  ethereum: 'https://img.cryptorank.io/coins/60x60.ethereum1524754015525.png',
-  tether: 'https://img.cryptorank.io/coins/60x60.tether1645007690922.png',
-  ripple: 'https://img.cryptorank.io/coins/60x60.xrp1634717634479.png',
-  bnb: 'https://img.cryptorank.io/coins/60x60.bnb1732530324407.png',
-  solana: 'https://img.cryptorank.io/coins/60x60.solana1606979093056.png',
-  usdcoin: 'https://img.cryptorank.io/coins/60x60.usd coin1634317395959.png',
-  dogecoin: 'https://img.cryptorank.io/coins/60x60.dogecoin1524754995294.png',
-  cardano: 'https://img.cryptorank.io/coins/60x60.cardano1524754132195.png',
-  tron: 'https://img.cryptorank.io/coins/60x60.tron1608810047161.png'
-};
-
-function createCryptoCard(crypto) {
-  const symbol = cryptoAbbreviations[crypto.key] || crypto.key.toUpperCase();
-  const roundedPrice = Number(crypto.price).toFixed(2); // دو رقم اعشار
-  return `
-    <div class="card crypto-card">
-      <div class="card-header">
-        <img class="crypto-icon" src="${cryptoIcons[crypto.key]}" alt="${symbol}">
-        <div class="card-title-group">
-          <div class="name">${crypto.key.toUpperCase()}</div>
-          <div class="nickname">${symbol}</div>
-        </div>
-      </div>
-      <div class="card-footer">
-        <div class="price">$${formatNumber(roundedPrice)}</div>
-      </div>
-    </div>
-  `;
-}
-
-loadCurrencyData();
-loadCryptoData();
-
-setInterval(() => {
+document.addEventListener('DOMContentLoaded', function() {
   loadCurrencyData();
-  loadCryptoData();
-}, 60000);
+  
+  setInterval(() => {
+    loadCurrencyData();
+  }, 60000);
+});
